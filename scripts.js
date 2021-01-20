5acb75c972de4c8c73b37c5d79ec0708869d10d1
spogsta <cheffjeff10@gmail.com>
2021-01-20 16:14:37 -0600

    fiddling

--- scripts.js
+++ scripts.js
@@ -1,24 +1,41 @@
 let turnCounter = 0;
 
+let playerXWins= 0;
+let playerOWins= 0;
+
 function playerTurn() {
     if (turnCounter % 2 ==0) {
-        return true;
-        return turnCounter +1;
+        return "O";
     }
     else {
-        return false;
-        return turncounter +1;
+        return "X";
+        
     }
+    
+}
+
+function checkWin() {
+    let cellArray = document.getElementsByClassName("cell");
+
+    if (cellArray[0,1,2].innerHTML=="X" || cellArray [0,1,2].innerHTML =="O"){
+        console.log("winner!")
+    }
+    else {};
+    return console.log(cellArray);
 }
 
 function checkGameState(){};//some sort of check if the max turns for draw, + checking if either player won
 
 function pushBtn(btn){
     
-
-    btn.innerHTML = 0;//placeholder for X'S and O'S
-    return console.log(btn);
-    
+    if (btn.innerHTML == "test "){
+        btn.innerHTML = playerTurn();//placeholder for X'S and O'S
+        turnCounter+=1;
+        checkWin();
+    }
+    else {
+        alert("no cheating!");
+    }
 
 };
 
